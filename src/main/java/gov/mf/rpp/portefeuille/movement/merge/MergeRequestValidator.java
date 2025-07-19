40f69b7a6c8aee414d70c774cd4627c0a73ade4b
khaled <khaledabderrahim512@gmail.com>
2025-07-19 20:29:31 +0100

    implement merge validation

--- src/main/java/gov/mf/rpp/portefeuille/movement/merge/MergeRequestValidator.java
+++ src/main/java/gov/mf/rpp/portefeuille/movement/merge/MergeRequestValidator.java
@@ -1,22 +1,35 @@
 package gov.mf.rpp.portefeuille.movement.merge;
 
+import gov.mf.rpp.portefeuille.PortefeuilleRepo;
+import jakarta.inject.Inject;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import java.util.Set;
 
 /**
  *
  * @author khaled
  */
-public class MergeRequestValidator implements ConstraintValidator<ValidMergeRequest, MergeRequest>{
+public class MergeRequestValidator implements ConstraintValidator<ValidMergeRequest, MergeRequest> {
 
-    /**
-     * check if new name/code aren't taken(rule for merging portefeuilles)
-     * check that there are no duplicates in target names
-     * check if all target portefeuille names do in fact exist and in an active state
-     */
+    @Inject
+    PortefeuilleRepo repo;
+
     @Override
-    public boolean isValid(MergeRequest value, ConstraintValidatorContext context) {
-        return true;
+    public boolean isValid(MergeRequest request, ConstraintValidatorContext context) {
+        //only unique names
+        var uniqueTargetNames = Set.copyOf(request.targetNames());
+        var uniqueTargetNamesSize = uniqueTargetNames.size();
+
+        //test that all target portefeuille do in fact exist and are active 
+        //by virtue if they have equal sizes than they contain the same names
+        if (uniqueTargetNamesSize == repo.activePortefeuilleNamesIn(uniqueTargetNames).size()) {
+
+            //test that name/code are not taken by other portefeuille that's out of the scope of
+            //this merging operation
+            return repo.validMergingTarget(request.name(), request.code(), uniqueTargetNames);
+        }
+        return false;
     }
-    
+
 }
